dcc0eed3ac12e05cfd0abdd14561da8092d7c637
Jeremy Sweitzer <jsweitzer@catalystdevworks.com>
2016-04-25 13:34:38 -0400

    Added linting to the project

--- BlogSite/gulpfile.js
+++ BlogSite/gulpfile.js
@@ -1,11 +1,14 @@
 ﻿"use strict";
 
-var _ = require('lodash'),
-    gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    cssmin = require('gulp-cssmin'),
-    rename = require('gulp-rename');
-
+var _ = require('lodash');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var plumber = require("gulp-plumber");
+var newer = require("gulp-newer");
+var watch = require("gulp-watch");
+var tslint = require("gulp-tslint");
 
 var angularJs = [
     './node_modules/angular2/bundles/angular2.dev.js',
@@ -82,3 +85,39 @@ gulp.task('copy-min-css', function () {
 
 gulp.task('default', ['copy-js', 'copy-css']);
 gulp.task('minify', ['copy-min-js', 'copy-min-css']);
+
+// Our TSLint Settings
+var TYPE_SCRIPT_REPORT = tslint.report("prose", {
+    emitError: false,
+    reportLimit: 50
+});
+
+var TYPE_SCRIPT_FILES = ["wwwroot/app/*.ts"];
+
+// The actual task to run
+gulp.task('TSLint:All', function () {
+    return gulp.src(TYPE_SCRIPT_FILES)
+        .pipe(plumber())
+        .pipe(tslint())
+        .pipe(TYPE_SCRIPT_REPORT);
+});
+
+// Listens for new (updated) typescript files and runs through TSlint
+gulp.task("TSLint:Newer", [], function (done) {
+    return gulp.src(TYPE_SCRIPT_FILES)
+        .pipe(plumber())
+        .pipe(newer(BIN))
+        .pipe(tslint())
+        .pipe(TYPE_SCRIPT_REPORT)
+        .pipe(gulp.dest(BIN));
+});
+
+//This task runs when the project opens. When any file changes, it will be run through TSLint
+gulp.task('TSLint:Watch', function () {
+    return gulp.src(TYPE_SCRIPT_FILES)
+        .pipe(watch(TYPE_SCRIPT_FILES))
+        .pipe(plumber())
+        .pipe(tslint())
+        .pipe(TYPE_SCRIPT_REPORT)
+        .pipe(gulp.dest(BIN));
+});
